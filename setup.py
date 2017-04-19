48082ac76919cca4ce19f7298a1a9686b3990141
yupeng <yupeng0921@gmail.com>
2017-04-19 05:29:28 +0000

    set version to 0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='dlvm',
-    version='0.0.1',
+    version='0.1.0',
     description='a distribute storage system base on lvm and iscsi',
     long_description=long_description,
     url='https://github.com/dlvm/dlvm',
